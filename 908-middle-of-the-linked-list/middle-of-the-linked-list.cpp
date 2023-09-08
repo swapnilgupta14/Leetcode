d40534fd6ea5cf9b84f0d9aaf09cc39929ad7396
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2023-09-08 19:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (56.41%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -17,22 +17,22 @@ public:
             count++;
             ptr = ptr->next;
         }
-        cout << "count" << count << endl;
+        // cout << "count" << count << endl;
         ptr = head;
-        if(count % 2 != 0){
+        // if(count % 2 != 0){
             count = count / 2;
             while (count--){
                 ptr = ptr->next;
             }
             return ptr;
-        }
-        else{
-            count = count / 2;
-            while (count--){
-                ptr= ptr->next;
-            }
-            return ptr;
-        }
+        // }
+        // else{
+        //     count = count / 2;
+        //     while (count--){
+        //         ptr= ptr->next;
+        //     }
+        //     return ptr;
+        // }
         
     }
 };
