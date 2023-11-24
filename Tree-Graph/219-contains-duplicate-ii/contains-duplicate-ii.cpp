9231db4707d9dd7d761a1cc2bc8c89f6f6850100
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2023-11-24 14:02:26 +0530

    Time: 128 ms (68.31%) | Memory: 77.7 MB (66.02%) - LeetSync

--- Tree-Graph/219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ Tree-Graph/219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int n=nums.size();
-        unordered_map<int,int>mp;
-
-        for(int i=0;i<n;i++){
-            if(mp.find(nums[i])!=mp.end()){
-                if(abs(i-mp[nums[i]])<=k){
+        unordered_map <int,int> mp;
+        int n = nums.size();
+        for(int i =0 ; i<n ; i++){
+            if(mp.find(nums[i]) != mp.end()){
+                if(abs(i-mp[nums[i]])  <= k){
                     return true;
                 }
             }
-            mp[nums[i]]=i;
+            // else{
+                mp[nums[i]]=i;
+            // }
         }
         return false;
     }
