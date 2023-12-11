76c3bdd676ce016ad7d5599b76e6fa1b40786532
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2023-12-11 15:02:19 +0530

    Time: 4 ms (55.29%) | Memory: 14.1 MB (32.02%) - LeetSync

--- Tree-Graph/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ Tree-Graph/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,33 +12,32 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
         queue <pair<TreeNode*, pair<int,int>>> q;
-        map <int, map<int, multiset<int>>> nodeMap;
+        map <int, map<int, multiset<int>>> mp;
         q.push({root, {0,0}});
-        while(!q.empty()){
-            // int s = q.size();
-            auto p = q.front();
+        while (!q.empty()){
+            auto qsf = q.front();
             q.pop();
-            int x = p.second.first;
-            int y = p.second.second;
-            TreeNode *ptr = p.first;
-            nodeMap[x][y].insert(ptr->val);
-            if(ptr->left != nullptr){
+            TreeNode* ptr = qsf.first; 
+            int x = qsf.second.first;
+            int y = qsf.second.second;
+            mp[x][y].insert(ptr->val);
+            if(ptr ->left != nullptr){
                 q.push({ptr->left, {x-1, y+1}});
             }
-            if(p.first->right != nullptr){
+            if(ptr ->right != nullptr){
                 q.push({ptr->right, {x+1, y+1}});
             }
         }
-        vector<vector<int>> ans;
-        for (auto i : nodeMap){
+
+        for (auto i : mp){
             vector<int>col;
-            for (auto m : i.second){
-                col.insert(col.end(), m.second.begin() , m.second.end());
+            for (auto j : i.second){
+                col.insert(col.end(), j.second.begin() , j.second.end());
             }
             ans.push_back(col);
         }
         return ans;
-        
     }
 };
