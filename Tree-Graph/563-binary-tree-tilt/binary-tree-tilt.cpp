6c20bcdcbcc366dbadd74d37c093ae4be977df1c
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2023-12-17 18:13:36 +0530

    Time: 7 ms (96.98%) | Memory: 23.9 MB (67.80%) - LeetSync

--- Tree-Graph/563-binary-tree-tilt/binary-tree-tilt.cpp
+++ Tree-Graph/563-binary-tree-tilt/binary-tree-tilt.cpp
@@ -1,15 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int tiltSum = 0;
+    int sum = 0;
     int findTilt(TreeNode* root) {
-        DFS(root);
-        return tiltSum;
+        if(root==nullptr){
+            return 0;
+        }
+        find(root);
+        return sum;
     }
-    int DFS(TreeNode* root) {
-        if(!root) return 0;
-        int leftSum = DFS(root -> left);                // sum of left subtree
-        int rightSum = DFS(root -> right);              // sum of right subtree
-        tiltSum += abs(leftSum - rightSum);             // add tilt of current node to overall tiltSum
-        return leftSum + rightSum + root -> val;        // returns sum of subtree starting at 'root'
+    int find(TreeNode *node){
+        if(node == nullptr){
+            return 0;
+        }
+        int lh = find(node->left);
+        int rh = find(node->right);
+        sum += abs(lh - rh);
+        return lh + rh + node->val;
     }
 };
